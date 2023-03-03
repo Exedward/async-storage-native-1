f346297dae20d52401d55cfea14b36d369f49357
Exedward <eduardo.ex7@gmail.com>
2023-03-03 15:55:02 -0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 
 export default function App() {
+
+  const [textoEntrada, SetTextoEntrada] = useState('')
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
+      <Text style={styles.titulo}>AsyncStorage</Text>
+      <TextInput
+        style={styles.entrada}
+        value={textoEntrada}
+        onChangeText={SetTextoEntrada}
+        autoCapitalize='words'
+        autoCorrect={false}
+        placeholder='Coloque o texto aqui'
+        placeholderTextColor='#888'
+        textAlign='center'
+        color='#ddd'
+      />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  entrada:{
+    width: '70%',
+    height: '7%',
+    borderBottomColor: '#ddd',
+    borderBottomWidth: 1,
+    padding: 10,
+    fontSize: 15,
+  },
+  titulo:{
+    marginBottom: '15%',
+    fontSize: 30,
+    fontWeight: 'bold',
+    //color: '#ddd',
+  }
 });
